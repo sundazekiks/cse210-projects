574e3cecb86429b9f5f0ed2c892cbb8006e51edf
sundazekiks <abutontravis1@gmail.com>
2025-09-03 17:48:43 -0400

    Exercise 3 Core Requirement 1 done

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.Write("What is your guess? ");
+        int guessNumber = int.Parse(Console.ReadLine());
+
+        if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else if (guessNumber == magicNumber)
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        else
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
