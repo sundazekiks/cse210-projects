4e559e614a484994987dd8a028ca25740718b024
sundazekiks <abutontravis1@gmail.com>
2025-09-03 16:15:53 -0400

    Core Requirement 1 done

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        // Prompt user for grade percentage:
+        Console.Write("Enter your grade: ");
+        string grade_str = Console.ReadLine();
+        //convert grade str to an integer
+        int grade = int.Parse(grade_str);
+
+        // Determine which letter grade the user got
+        if (grade >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else if (grade < 60)
+        {
+            Console.WriteLine("F");
+        }
+
+
+
+
     }
 }
