d32d76e9028acca080b96c22ad0ee86c79c8c492
sundazekiks <abutontravis1@gmail.com>
2025-09-04 05:57:26 -0400

    Excercise 3 Core requirements and stretch challenge done!

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,30 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
         
         bool keep_asking = true;
 
+
         while (keep_asking)
         {
+
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            //setting a count variable of how many times the user has guessed
+            int count = 1;
+
+
+            // I just wrote this line of code I used it for debugging :: Console.WriteLine(magicNumber);
+
             Console.Write("What is your guess? ");
             int guessNumber = int.Parse(Console.ReadLine());
 
+            
             if (guessNumber == magicNumber)
             {
-                Console.Write("You guessed it!");
-                break;
+                Console.WriteLine($"You guessed it! after {count} times");
+
+                // asking user if they wanna go again
+                Console.Write("Wanna play again? ");
+                string answer = Console.ReadLine();
+
+                if (answer == "yes")
+                {
+                    keep_asking = true;
+                }
+                else
+                {
+                    keep_asking = false;
+                    Console.WriteLine("Thank you for playing!");
+                }
             }
             else if (guessNumber > magicNumber)
             {
                 Console.WriteLine("Lower");
-
+                count += 1;
             }
             else
             {
                 Console.WriteLine("Higher");
+                count += 1;
             }
         }
+
+        
     }
 }
